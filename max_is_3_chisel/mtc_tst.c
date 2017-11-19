4e5b96103a470b218e8e589db1e71beabe4c4ad8
Anton <tony.moiseew2012@yandex.ru>
2017-11-19 22:43:21 +0300

    сделал тест

--- max_is_3_chisel/mtc_tst.c
+++ max_is_3_chisel/mtc_tst.c
@@ -1,12 +1,34 @@
 #include <stdio.h>
+#include <stdlib.h>
 int mtc(int a,int b,int c);
 
 int main()
 {	
-	int a = 33;
-	int b = 731;
-	int c = 99;
-	int d = mtc(a,b,c);
-	printf("%d", d);
-	return d;
+	int a,b,c,i;
+	int answer_s;
+	int answer_c;
+	for (i = 0; i < 10000; i++){
+	a = rand() % 100 + 1;
+	b = rand() % 100 + 1;
+	b = rand() % 100 + 1;
+	answer_s = mtc(a,b,c);
+	answer_c = mtc_c(a,b,c);
+	if (answer_c != answer_s){
+	printf("%d\n", a);
+	printf("%d\n", b);
+	printf("%d\n", c);
+	printf("%d\n");
+	}
+	}
+	return 0;
+}
+	
+
+
+int mtc_c(int a,int b,int c)
+{	
+	if (a >= b && a >= c) return a;
+	if (b >= a && b >= c) return b;
+	if (c >= a && c >= b) return c;
+	return 0;
 }
