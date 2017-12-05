794fd38f4eac027d9e738bf8abd75db08a73ae78
Anton <tony.moiseew2012@yandex.ru>
2017-12-05 14:57:11 +0300

    исправил

--- vectors/cvc.s
+++ vectors/cvc.s
@@ -5,16 +5,19 @@ data ".MyData"
 	global B: word[4]=(float(2),float(12),float(6),float(8));
 end ".MyData";
 
-
+nobits ".MyData1"
+	global C: word[4];
+end ".MyData1";
 
 
 begin ".text"
 <_cvc>
 	push ar0, gr0;
-	push ar1; gr1;
-	push ar2; gr2;
+	push ar1, gr1;
+	push ar2, gr2;
 	ar0 = A;
 	ar1 = B;
+	ar2 = C;
 	fpu 0 rep 2 vreg0 = [ar0++];
 	fpu 0 rep 2 vreg1 = [ar1++];
 	fpu 0 .float vreg2 = vreg0+vreg1;
