30de9a2466599a8f245973d78b4bfdc26930bddd
Anton <tony.moiseew2012@yandex.ru>
2017-12-19 13:01:29 +0300

    все сделано и протесчено

--- rgb/rgb.s
+++ rgb/rgb.s
@@ -19,63 +19,107 @@ begin ".text"
 	push ar2, gr2;
 	push ar3, gr3;
 	push ar4, gr4;
+	push ar5, gr5;
 	gr0 = 6;  		//в gr0 загрузили интервал
 	gr2 = 0;
 	gr3 = 0;
-	gr4 = 10;
+	gr4 = 32;
+	gr5 = 0;
 	ar1 = C;
 	ar2 = D;
 	ar3 = E;
 	gr1 = [--ar4];
-<counterdes>			//цикл,вычисляющий количество дестков и остататок в числе в gr2-десятки,в gr3- остаток
+<counterdes>			//цикл,вычисляющий количество 32 и остаток в числе в gr2-тридцатки,в gr3- остаток
 	gr1 = gr1-gr4;
 	if > goto help_counterdes1;
 	if < goto exit_counterdes ;
 <exit_counterdes>
-	gr3 = gr1 + gr4;		
+	gr3 = gr1 + gr4;
+				//		
 	gr3 = gr3 - 1;			//часть кода,которая сортирует тройки в размере остатка
-	vlen = gr1;
-	ar0 = [--ar4];		//в vlen загрузили количество троек
+	vlen = gr3;
+	ar0 = [--ar4];			//в vlen загрузили количество троек
 	fpu 0 rep vlen vreg0 = [ar0++gr0];
 	fpu 0 rep vlen [ar1++] = vreg0;
-					//ar4 = ar7 -12;
-					//ar0 = [--ar4];
-					//ar0 = [--ar4];
-					//ar0 = ar0 + 2;
-					//fpu 0 rep vlen vreg0 = [ar0++gr0];
-					//fpu 0 rep vlen [ar2++] = vreg0;
-					//ar4 = ar7 -12;
-					//ar0 = [--ar4];
-					//ar0 = [--ar4];
-					//ar0 = ar0 + 4;
-					//fpu 0 rep vlen vreg0 = [ar0++gr0];
-					//fpu 0 rep vlen [ar3++] = vreg0;
-
+					
 <sort_des>			//часть кода,которая сортирует тройки в размере десятков
-	gr2 = gr2 - 1;
+	gr2 = gr2 - gr5;
 	if > goto help_sort_des;
-<help_sprt_des>
-	ar4 = ar7 - 12;
-	ar0 = [--ar4];		
-	fpu 0 rep 10 vreg0 = [ar0++gr0];
-	fpu 0 rep 10 [ar1++] = vreg0;
-					//ar4 = ar7 -12;
-					//ar0 = [--ar4];
-					//ar0 = [--ar4];
-					//ar0 = ar0 + 2;
-					//fpu 0 rep 10 vreg0 = [ar0++gr0];
-					//fpu 0 rep 10 [ar2++] = vreg0;
-					//ar4 = ar7 -12;
-					//ar0 = [--ar4];
-					//ar0 = [--ar4];
-					//ar0 = ar0 + 4;
-					//fpu 0 rep 10 vreg0 = [ar0++gr0];
-					//fpu 0 rep 10 [ar3++] = vreg0;
-
+	goto cicle2;
+<help_sort_des>
+		
+	fpu 0 rep 32 vreg0 = [ar0++gr0];
+	fpu 0 rep 32 [ar1++] = vreg0;
+	gr2 = gr2 - 1;
 	goto sort_des;
 
-	
+
+
+
+
+
+
+<cicle2>			// часть кода,где сортируются вторые тройки
+	gr2 = 0;
+	gr3 = 0;
+	ar4 = ar7 - 14;
+	gr1 = [--ar4];
+<counterdes_1>			
+	gr1 = gr1-gr4;
+	if > goto help_counterdes1_1;
+	if < goto exit_counterdes_1 ;
+<exit_counterdes_1>
+	gr3 = gr1 + gr4;		
+	gr3 = gr3 - 1;			
+	vlen = gr3;
+	ar0 = [--ar4];			
+	ar0 = ar0 + 2;
+	fpu 0 rep vlen vreg0 = [ar0++gr0];
+	fpu 0 rep vlen [ar2++] = vreg0;					
+<sort_des_1>			
+	gr2 = gr2 - gr5;
+	if > goto help_sort_des_1;
+	goto cicle3;
+<help_sort_des_1>
+		
+	fpu 0 rep 32 vreg0 = [ar0++gr0];
+	fpu 0 rep 32 [ar2++] = vreg0;
+	gr2 = gr2 - 1;
+	goto sort_des_1;
+
+
+
+
+<cicle3>			// часть кода,где сортируются третьи тройки
+	gr2 = 0;
+	gr3 = 0;
+	ar4 = ar7 - 14;
+	gr1 = [--ar4];
+<counterdes_2>			
+	gr1 = gr1-gr4;
+	if > goto help_counterdes1_2;
+	if < goto exit_counterdes_2 ;
+<exit_counterdes_2>
+	gr3 = gr1 + gr4;		
+	gr3 = gr3 - 1;			
+	vlen = gr3;
+	ar0 = [--ar4];			
+	ar0 = ar0 + 4;
+	fpu 0 rep vlen vreg0 = [ar0++gr0];
+	fpu 0 rep vlen [ar3++] = vreg0;					
+<sort_des_2>			
+	gr2 = gr2 - gr5;
+	if > goto help_sort_des_2;
+	goto exit_rgb;
+<help_sort_des_2>
+		
+	fpu 0 rep 32 vreg0 = [ar0++gr0];
+	fpu 0 rep 32 [ar3++] = vreg0;
+	gr2 = gr2 - 1;
+	goto sort_des_2;
+		
 <exit_rgb>
+	pop ar5, gr5;
 	pop ar4, gr4;
 	pop ar3, gr3;
 	pop ar2, gr2;
@@ -87,4 +131,10 @@ begin ".text"
 <help_counterdes1>
 	gr2 = gr2+1;
 	goto counterdes;
+<help_counterdes1_1>
+	gr2 = gr2+1;
+	goto counterdes_1;
+<help_counterdes1_2>
+	gr2 = gr2+1;
+	goto counterdes_2;
 end ".text";
--- rgb/rgb_tst.c
+++ rgb/rgb_tst.c
@@ -1,9 +1,9 @@
 
 void rgb(int num, double *array);
-double array[9] = {6,9,3,4,5,8,11,12,13};
+double array[36] = {6,9,3,4,5,8,11,12,13,1,2,3,4,5,6,7,8,9,0,1,2,3,5,7,5,4,3,1,655,10,11,11,33,65,32,12};
 int main()
 {	
-	int num = 24;
+	int num = 12;
 	rgb(num,array);
 	return 0;
 }
