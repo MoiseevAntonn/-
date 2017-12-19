20727b3a00cefc34ddbeb42055c3191b8ac6139a
Anton <tony.moiseew2012@yandex.ru>
2017-12-19 11:38:17 +0300

    что то новое

--- rgb/rgb.s
+++ rgb/rgb.s
@@ -20,27 +20,60 @@ begin ".text"
 	push ar3, gr3;
 	push ar4, gr4;
 	gr0 = 6;  		//в gr0 загрузили интервал
+	gr2 = 0;
+	gr3 = 0;
+	gr4 = 10;
 	ar1 = C;
 	ar2 = D;
 	ar3 = E;
-	gr1 = [--ar4];		
-	gr1 = gr1 - 1;
+	gr1 = [--ar4];
+<counterdes>			//цикл,вычисляющий количество дестков и остататок в числе в gr2-десятки,в gr3- остаток
+	gr1 = gr1-gr4;
+	if > goto help_counterdes1;
+	if < goto exit_counterdes ;
+<exit_counterdes>
+	gr3 = gr1 + gr4;		
+	gr3 = gr3 - 1;			//часть кода,которая сортирует тройки в размере остатка
 	vlen = gr1;
 	ar0 = [--ar4];		//в vlen загрузили количество троек
 	fpu 0 rep vlen vreg0 = [ar0++gr0];
 	fpu 0 rep vlen [ar1++] = vreg0;
-	ar4 = ar7 -12;
-	ar0 = [--ar4];
-	ar0 = [--ar4];
-	ar0 = ar0 + 2;
-	fpu 0 rep vlen vreg0 = [ar0++gr0];
-	fpu 0 rep vlen [ar2++] = vreg0;
-	ar4 = ar7 -12;
-	ar0 = [--ar4];
-	ar0 = [--ar4];
-	ar0 = ar0 + 4;
-	fpu 0 rep vlen vreg0 = [ar0++gr0];
-	fpu 0 rep vlen [ar3++] = vreg0;
+					//ar4 = ar7 -12;
+					//ar0 = [--ar4];
+					//ar0 = [--ar4];
+					//ar0 = ar0 + 2;
+					//fpu 0 rep vlen vreg0 = [ar0++gr0];
+					//fpu 0 rep vlen [ar2++] = vreg0;
+					//ar4 = ar7 -12;
+					//ar0 = [--ar4];
+					//ar0 = [--ar4];
+					//ar0 = ar0 + 4;
+					//fpu 0 rep vlen vreg0 = [ar0++gr0];
+					//fpu 0 rep vlen [ar3++] = vreg0;
+
+<sort_des>			//часть кода,которая сортирует тройки в размере десятков
+	gr2 = gr2 - 1;
+	if > goto help_sort_des;
+<help_sprt_des>
+	ar4 = ar7 - 12;
+	ar0 = [--ar4];		
+	fpu 0 rep 10 vreg0 = [ar0++gr0];
+	fpu 0 rep 10 [ar1++] = vreg0;
+					//ar4 = ar7 -12;
+					//ar0 = [--ar4];
+					//ar0 = [--ar4];
+					//ar0 = ar0 + 2;
+					//fpu 0 rep 10 vreg0 = [ar0++gr0];
+					//fpu 0 rep 10 [ar2++] = vreg0;
+					//ar4 = ar7 -12;
+					//ar0 = [--ar4];
+					//ar0 = [--ar4];
+					//ar0 = ar0 + 4;
+					//fpu 0 rep 10 vreg0 = [ar0++gr0];
+					//fpu 0 rep 10 [ar3++] = vreg0;
+
+	goto sort_des;
+
 	
 <exit_rgb>
 	pop ar4, gr4;
@@ -49,4 +82,9 @@ begin ".text"
 	pop ar1, gr1;
 	pop ar0, gr0;
 	return;
+
+
+<help_counterdes1>
+	gr2 = gr2+1;
+	goto counterdes;
 end ".text";
--- rgb/rgb_tst.c
+++ rgb/rgb_tst.c
@@ -3,7 +3,7 @@ void rgb(int num, double *array);
 double array[9] = {6,9,3,4,5,8,11,12,13};
 int main()
 {	
-	int num = 3;
+	int num = 24;
 	rgb(num,array);
 	return 0;
 }
